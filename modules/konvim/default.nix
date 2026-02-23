00796379dd84d8cc693d00a0f3fa72c38ccde232
KN13KOMETA <kn13kometa@gmail.com>
2026-02-23 23:16:58 +0300

    Remove spell highlights

--- modules/konvim/default.nix
+++ modules/konvim/default.nix
@@ -9,7 +9,6 @@
       # NOTE: Disable `spellfile.vim`, we manage spell files with flakes instead
       luaConfigRC.disable-spellfile-plugin = "vim.g.loaded_spellfile_plugin = 1";
       spellcheck = {
-        enable = true;
         languages = [
           "en"
           "ru"
