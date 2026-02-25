951e34ab4e81f6b766aafc7f5ec5833012b5241b
KN13KOMETA <kn13kometa@gmail.com>
2026-02-25 18:43:45 +0300

    Clean autocomplete

--- modules/konvim/autocomplete/default.nix
+++ modules/konvim/autocomplete/default.nix
@@ -3,9 +3,12 @@
   # NOTE: For now I am sticking with nvim-cmp, maybe check blink.cmp later
   config.vim.autocomplete.nvim-cmp = {
     enable = true;
+    # NOTE: cmp-nvim-lsp turned on automatically
     sourcePlugins = [
-      "cmp-nvim-lsp"
       "cmp-path"
     ];
+    sources = {
+      path = "[Path]";
+    };
   };
 }
