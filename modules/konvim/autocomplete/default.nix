a183b77c1149d8adeb45afcc8450f84560a68d5c
KN13KOMETA <kn13kometa@gmail.com>
2026-04-11 20:45:00 +0300

    Force sourcePlugins so treesitter doesnt get included

--- modules/konvim/autocomplete/default.nix
+++ modules/konvim/autocomplete/default.nix
@@ -3,8 +3,8 @@
   # NOTE: For now I am sticking with nvim-cmp, maybe check blink.cmp later
   config.vim.autocomplete.nvim-cmp = {
     enable = true;
-    # NOTE: cmp-nvim-lsp turned on automatically
-    sourcePlugins = [
+    sourcePlugins = inputs.nixpkgs.lib.mkForce [
+      "cmp-nvim-lsp"
       "cmp-path"
     ];
     sources = {
